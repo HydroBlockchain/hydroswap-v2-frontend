dcd1e0ab0c4f4feac6d75c923792ba1d7a6b82a9
okeken <okekehinde@gmail.com>
2022-05-31 11:50:45 +0200

    project init

---
+++ packages/uikit/src/react-app-env.d.ts
@@ -0,0 +1,12 @@
+interface Window {
+  ethereum?: {
+    isMetaMask?: true;
+    isOpera?: true;
+    isTrust?: true;
+    providers?: any[];
+    request?: (...args: any[]) => Promise<void>;
+  };
+  BinanceChain?: {
+    bnbSign?: (address: string, message: string) => Promise<{ publicKey: string; signature: string }>;
+  };
+}
