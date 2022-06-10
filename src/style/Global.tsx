cb12852914478a192396263730f78829b69007cd
Pelumi527 <fatolupelumi@gmail.com>
2022-06-10 10:15:09 +0100

    stake

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -8,7 +8,7 @@ declare module 'styled-components' {
 
 const GlobalStyle = createGlobalStyle`
   * {
-    font-family: 'Montserrat', Droidiga, sans-serif;
+    font-family: 'Montserrat', sans-serif;
   }
   body {
     background-color: ${({ theme }) => theme.colors.background};
