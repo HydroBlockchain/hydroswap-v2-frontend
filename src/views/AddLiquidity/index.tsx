707e742ecab7b98798596307cf03213778bf27f4
ux33-331 <61588635+ux33-331@users.noreply.github.com>
2022-06-13 15:21:58 +0200

    small fix

--- src/views/AddLiquidity/index.tsx
+++ src/views/AddLiquidity/index.tsx
@@ -290,7 +290,7 @@ export default function AddLiquidity() {
       <AppBody>
         <AppHeader
           title={t('Add Liquidity')}
-          subtitle={t('Add liquidity to receive LP tokens')}
+          subtitle={t('Add liquidity to receive Droplet tokens')}
           helper={t(
             'Liquidity providers earn a 0.17% trading fee on all trades made for that token pair, proportional to their share of the liquidity pool.',
           )}
